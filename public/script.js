17271908d8603c4a1bd575e4479c3f35c921d1d3
taramouse <nickykeyse@gmail.com>
2018-10-30 20:30:20 +0000

    Added cart logic for item qty.

--- public/script.js
+++ public/script.js
@@ -3,10 +3,10 @@ new Vue({
   data: {
     total: 0,
     items: [
-      {title: 'Item 1'},
-      {title: 'Item 2'},
-      {title: 'Item 3'},
-      {title: 'Item 4'}
+      {id: 1, title: 'Item 1'},
+      {id: 2, title: 'Item 2'},
+      {id: 3, title: 'Item 3'},
+      {id: 4, title: 'Item 4'}
     ],
     cart: []
   },
@@ -14,7 +14,22 @@ new Vue({
     addItem: function(index) {
       console.log(index);
       this.total += 9.99;
-      this.cart.push(this.items[index]);
+      // this.cart.push(this.items[index]);
+      var item = this.items[index];
+      var found = false;
+      for(var i = 0; i < this.cart.length; i++) {
+        if(this.cart[i].id === item.id) {
+          found = true;
+          this.cart[i].qty++;
+        }
+      }
+      if(!found) {
+          this.cart.push({
+          id: item.id,
+          title: item.title,
+          qty: 1
+        });
+      }
     }
   }
 });
